db02b99f37715ea87e593944db4ebab04f26d201
Matt Brooks <mattbrooks2010@gmail.com>
2023-07-23 03:49:31 -0500

    Code Challene

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@
 
 // Write code that will return a random letter from you name
 
-console.log()
+const anyName = "Jonathan";
+// What about the number?
+// Do you see the problem???
+console.log("Matt".charAt(1, 4));
+console.log("Matt".charAt(Math.floor(Math.random() * 4)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
