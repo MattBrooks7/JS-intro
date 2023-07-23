0a5440f724b857ed501403793637ec492fea2cf7
Matt Brooks <mattbrooks2010@gmail.com>
2023-07-23 03:38:24 -0500

    start

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-// Math Methods and Properties
-console.log(Math.PI);
-console.log(Math.trunc(Math.PI));
-console.log(Math.ceil(Math.PI));
-console.log(Math.pow(2, 10));
-console.log(Math.min(2, 5, 9));
-console.log(Math.min(2, 5, 9));
-console.log(Math.max(2, 5, 9));
-console.log(Math.random());
-console.log(Math.floor(Math.random() * 10) + 1);
+// Your First Code Challenge
 
+// Write code that will return a random letter from you name
+
+console.log()
