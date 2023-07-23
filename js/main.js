c61c14260df31feff253bd4bf87889d544fa8499
Matt Brooks <mattbrooks2010@gmail.com>
2023-07-23 02:52:55 -0500

    Numbers

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Strings
+const myVariable = "Mathematics";
+
+// The length property
+console.log("Every good boy does fine".length);
+
+// String Methods
+console.log(myVariable.charAt(6));
+console.log(myVariable.indexOf("ath"));
+console.log(myVariable.slice(5, 6));
+console.log(myVariable.toLowerCase())
+console.log(myVariable.includes("mat"));
+console.log(myVariable.split("e"));
