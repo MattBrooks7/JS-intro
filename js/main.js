8984978500d32c0e650a6d8d5b3d32a59d6876e3
Matt Brooks <mattbrooks2010@gmail.com>
2023-07-23 17:53:26 -0500

    JSON

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
-// Javascript Classes
-class Pizza {
-  crust = "original";
-  #sauce = "traditional";
-  #size;
-  constructor(pizzaSize) {
-    this.#size = pizzaSize;
-  }
-  getCrust() {
-    return this.crust
-  }
-  setCrust(pizzaCrust) {
-    this.crust = pizzaCrust;
-  }
-  hereYouGo() {
-    console.log(
-      `Here's your ${this.crust} ${this.#sauce} sauce ${this.#size} pizza.`
-    );
-  }
-}
+// JSON: Javascript Object Notation
+/*
+JSON is used to send and receive data.
+JSON is a text format that is completely language independent.
+Meaning JSON is used to send & recieve data in many languages ... not just in JavaScript
+*/
 
-const myPizza = new Pizza("large");
-myPizza.hereYouGo();
-console.log(myPizza.getCrust());
+const myObj = {
+  name: "Dave",
+  hobbies: ["eat", "sleep", "code"],
+  hello: function() {
+    console.log("Hello!");
+  }
+};
+
+console.log(myObj);
+console.log(myObj.name);
+myObj.hello();
+console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj);
+console.log(sendJSON);
+console.log(typeof sendJSON);
+console.log(sendJSON.name);
+
+const receiveJSON = JSON.parse(sendJSON);
+console.log(receiveJSON);
+console.log(typeof receiveJSON);
