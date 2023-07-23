06a3d3f3050fad78ca4281a813c20d175512a4cc
Matt Brooks <mattbrooks2010@gmail.com>
2023-07-23 14:20:04 -0500

    merged

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
-// LOOPS
-let name = "Dave";
-let counter = 0;
-let myLetter;
-while (counter <= 3) {
-    myLetter = name[counter];
-    console.log(myLetter);
-    if (counter === 2) {
-        counter += 2;
-        continue;
-    }
-    if (myLetter === "v") break;
-    counter++;
-}
+// Your First Interactive Game
+let playGame = confirm("Shall we play rock, paper, or scissors?");
+if (playGame) {
+  //play
+  let playerChoice = prompt("Please enter rock, paper, or scissors.");
+  if (playerChoice) {
+    let playerOne = playerChoice.trim().toLowerCase();
+    if (
+      playerOne === "rock" ||
+      playerOne === "paper" ||
+      playerOne === "scissors"
+    ) {
+      let computerChoice = Math.floor(Math.random() * 3 + 1);
+      let computer =
+        computerChoice === 1
+          ? "rock"
+          : computerChoice === 2
+          ? "paper"
+          : "scissors";
 
-console.log(counter)
+      let result =
+        playerOne === computer
+          ? "Tie game!"
+          : playerOne === "rock" && computer === "paper"
+          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+          : playerOne === "paper" && computer === "scissors"
+          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+          : playerOne === "scissors" && computer === "rock"
+          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+          : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+      alert(result);
+      let playAgain = confirm("Play Again?");
+      playAgain ? location.reload() : alert("Ok, thanks for playing.");
+    } else {
+      alert("You didn't enter rock, paper, or scissors.");
+    }
+  } else {
+    alert("I guess you changed your mind. Maybe next time.");
+  }
+} else {
+  alert("Ok, maybe next time.");
+}
